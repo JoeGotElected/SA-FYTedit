bf751fb313bd2533fd59c1b5b6b9bad6780b214e
JoeGotElected <100154714+JoeGotElected@users.noreply.github.com>
2022-02-21 12:44:14 -0500

    Update connect.lua

--- connect.lua
+++ connect.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/main/mainExploit.lua?token=GHSAT0AAAAAABRZ2K6R2GOZGBDIYEUUZIJWYQTZTQQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/main/spychat.lua"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/main/spychat.lua?token=GHSAT0AAAAAABRZ2K6RUD6KMVK2TDDZCOJOYQTZWEA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/main/mainExploit.lua"))()
