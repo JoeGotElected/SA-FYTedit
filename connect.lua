25436dc6b91e5b2707ae600c37f8eb2ee1573a89
JoeGotElected <100154714+JoeGotElected@users.noreply.github.com>
2022-02-22 15:58:22 -0500

    Update connect.lua

--- connect.lua
+++ connect.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/spychat.lua"))()
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/mainExploit.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/infyield.lua"))()
