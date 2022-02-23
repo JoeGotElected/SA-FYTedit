c5af8a39570ad7f6251a7f10a1b035005da7a220
JoeGotElected <skeidoxofficial@gmail.com>
2022-02-23 16:25:16 -0500

    Update connect.lua

--- connect.lua
+++ connect.lua
@@ -6,4 +6,3 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTe
 
 wait(25)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/wedontcare.lua"))()
-s
