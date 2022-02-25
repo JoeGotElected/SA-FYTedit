fd4ea788d2c5c359ec3fe3a7f47840e63dfe84ff
JoeGotElected <skeidoxofficial@gmail.com>
2022-02-24 22:01:07 -0500

    newchatspamtest

--- wedontcare.lua
+++ wedontcare.lua
@@ -1,13 +1,4 @@
 print("VERSION 1.0.7", "All")
 wait(15)
-while true do
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Just a friendly reminder, us 'exploiters' are here to ruin your experience!", "All")
-wait(3)
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("No need to scream 'clipped' or 'recorded' as were able to get alts with one click. Feel free to report us, whats stoppin ya?", "All")
-wait(3)
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Keep in mind, not all exploiters are the same. We don't care what you think or how you feel.", "All")
-wait(3)
-game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Feel free trying to fight us! We don't mind a fun fight as long as you don't mind your KDR dropping!", "All")
-wait(30)
-end
+
 
