084c7975eb586d189b041b16923c7537fa0aa655
JoeGotElected <skeidoxofficial@gmail.com>
2022-02-25 23:35:52 -0500

    Update connect.lua

--- connect.lua
+++ connect.lua
@@ -6,7 +6,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTe
 print("2/4")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/infyield.lua"))()
 print("3/4")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/chatspammer.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/chatspammer.lua"))()
 print("4/4")
 wait(1)
 print("all code has been launched succsesfully!")
