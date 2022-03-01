ba65dfd8acb789d9df2845ab29a2c99c88b355c3
JoeGotElected <skeidoxofficial@gmail.com>
2022-03-01 06:42:37 -0500

    Infinite yield fix

--- connect.lua
+++ connect.lua
@@ -2,10 +2,13 @@ print("Script loading... 0/3")
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/spychat.lua"))()
 print("1/3")
+wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/mainExploit.lua"))()
 print("2/3")
+wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JoeGotElected/SA-FYTedit/UI/infyield.lua"))()
 print("3/3")
+wait(1)
 print("all code has been launched succsesfully!")
 
 
